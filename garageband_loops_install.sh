c93e02e3fe58e8d4605c36df6c781dc3cb2f8601
horizontalz <zinkotheclown@yahoo.com>
2015-05-15 23:35:57 -0700

    Update and rename garageband_loops_extraction.sh to garageband_loops_install.sh

--- garageband_loops_install.sh
+++ garageband_loops_install.sh
@@ -1,3 +1,4 @@
-#!bin/sh
+#!/bin/sh
+## After copying loops, use this script to batch install the packages.
 
 find /Library/GarageBand\ Loops\ I/ -iname "*.pkg" -exec installer -pkg '{}' -target / \;
