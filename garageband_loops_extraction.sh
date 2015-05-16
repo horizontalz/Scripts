f5b7c838d920485ce5d748e26f435d42457d474e
horizontalz <zinkotheclown@yahoo.com>
2015-05-15 23:41:29 -0700

    Create garageband_loops_extraction.sh

---
+++ garageband_loops_extraction.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+find /private/var/folders/*/*/C/com.apple.garageband10/com.apple.MusicApps/audiocontentdownload.apple.com -type f -name "*.pkg" -exec cp -rv '{}' /Users/techsupport/Desktop/GarageBand\ Loops  \;
