29aa5fb01c1ae2481ad1c8974604c1f87790256b
horizontalz <zinkotheclown@yahoo.com>
2016-01-17 17:50:50 -0800

    Create bashloop2.sh

---
+++ bashloop2.sh
@@ -0,0 +1,23 @@
+cat <<EOF >/foo.txt
+/Applications/Chess.app
+/Applications/Automator.app
+/Applications/Cisco
+/Applications/Freebird.app
+/Applications/Maya.app
+/Applications/Blackmagic Disk Speed Test.app
+EOF
+
+#Removes Windows style carriage returns in copied textile
+tr '\r' '\n' </foo.txt> /foo2.txt
+
+RES=$(cat /foo2.txt)
+IFS=$'\n'       # make newlines the only separator
+for i in $RES
+do 
+if [ ! -d $i ]; then
+echo "$i" >> /foo3.txt
+##else 
+##echo "not missing"
+##jamf install -package "$PKG" -path $DISTROPOINT/"$PKG" -target /
+fi
+done
